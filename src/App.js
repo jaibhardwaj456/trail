868254e15b42c85b1c9396602ff8b83e114e865c
Jai <jaibhardwaj456@gmail.com>
2023-10-19 17:58:15 +0530

    yeo

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ const App = () => {
     const [movies,setMovies] = useState([]);
     const [searchTerm,setSearchTerm] = useState('');
     const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
         alert('Data Found');
+        const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
         setMovies(data.Search);
     }
